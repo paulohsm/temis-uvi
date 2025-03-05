572c63615864130b860ee9e06dfbc2c7b89ff5bb
paulohsm <paulohsm@gmail.com>
2025-03-05 01:57:54 -0300

    Adicionados comentários ao código "decadas.py".

--- decadas.py
+++ decadas.py
@@ -2,9 +2,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
+# Carregando dados
 caminho = "/home/santiago/Projetos/temis-uvi/dados/Maracanau.csv"
 uv = pd.read_csv(caminho, skiprows=1, na_values=-999.0, parse_dates=['data'])
 
+# Metadados para utilização nos gráficos
 siglas = uv.columns[1:].values
 unidades = ['Índice UV', 'kJ/m2', 'kJ/m2', 'kJ/m2'] # 1  [ 1 UV index unit equals 25 mW/m2 ]
 descricoes = ['Índice UV ao meio dia local', 
@@ -14,6 +16,7 @@ descricoes = ['Índice UV ao meio dia local',
 minimos = [0., 0., 0., 0.]
 maximos = [18., 18., 18., 18.]
 
+# Dicionário com metadados das variáveis
 variaveis = {
     'sigla': siglas, 
     'unidade': unidades,
@@ -22,34 +25,47 @@ variaveis = {
     'maximo': maximos
 }
 
-uv['decada'] = (uv['data'].dt.year // 10) * 10
-uv['mes_na_decada'] = (uv['data'].dt.year % 10) * 12 + uv['data'].dt.month
-
-decadas = uv.groupby(['decada', 'mes_na_decada']).agg(['mean', 'std'])
-
+# Rótulos de meses nos gráficos
 rotulos_meses = ['M', 'J', 'S', 'D'] * 10
 posicoes_meses = np.arange(3, 121, 3)
 
-plt.figure(figsize=(19.2, 10.8), dpi=320)
+# Novas colunas para organizar por década e por mês
+uv['decada'] = (uv['data'].dt.year // 10) * 10
+uv['mes_na_decada'] = (uv['data'].dt.year % 10) * 12 + uv['data'].dt.month
+
+# Calculando médias e desvios padrão mensais
+decadas = uv.groupby(['decada', 'mes_na_decada']).agg(['mean', 'std'])
+
+# Gerando os gráficos, por variável
 for i, variavel in enumerate(variaveis['sigla']):
     plt.figure(figsize=(19.2, 10.8), dpi=320)
+
+    # Linhas para delimitar os anos
     for ano in range(12, 121, 12):
         plt.axvline(x=ano, color='gray', linestyle='--', linewidth=0.5)
+
+    # Customizações
     plt.xticks(posicoes_meses, rotulos_meses)
     plt.title(f'{variaveis['descricao'][i]} (médias e desvios padrão - décadas sobrepostas)')
     plt.xlim(1, 120)
     plt.ylabel(variaveis['unidade'][i])
     plt.ylim(variaveis['minimo'][i], variaveis['maximo'][i])
     plt.tight_layout()
+
+    # Gerando as curvas para cada década
     for decada in decadas.index.levels[0][0:6]:
         dados_decada = decadas.loc[decada, variavel]
         plt.plot(dados_decada.index, dados_decada['mean'], label=f'{decada}s')
+
+        # Áreas correspondentes aos desvios padrão
         plt.fill_between(
             dados_decada.index,
             dados_decada['mean'] - dados_decada['std'],
             dados_decada['mean'] + dados_decada['std'],
             alpha=0.2
         )
+    
+    # Acabamento final e salvamento do arquivo do gráfico
     plt.legend()
     plt.savefig(f'graficos/decadas_{variavel}.png')
     plt.close()
